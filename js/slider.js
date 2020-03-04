0a5c0e8733c568a53296641b2cd800cf918865ab
Mirian <garciaivonne11@gmail.com>
2020-03-04 02:26:00 -0600

    agrega font slick

--- js/slider.js
+++ js/slider.js
@@ -1,11 +1,14 @@
 $(document).ready(function () {
   const config = {
+    arrows: false,
+    pauseOnFocus: false,
+    pauseOnHover: false,
     infinite: true,
     speed: 500,
     slidesToShow: 4,
-    slidesToScroll: 1,
+    slidesToScroll: 2,
     autoplay: true,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 3000,
 
     responsive: [{
         breakpoint: 768,
@@ -19,5 +22,25 @@ $(document).ready(function () {
       // instead of a settings object
     ]
   }
-  $('.slider-canales').slick(config);
+  $('.slider-canales').slick({
+    arrows: false,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+
+    responsive: [{
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
 });
