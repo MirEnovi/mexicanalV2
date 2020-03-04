4c328f25a1bcd63b6bd69a4ed4c5a579da107e10
Mirian <mir@teoapp.co>
2020-03-03 21:36:43 -0600

    agrega descarga a seccion mxcan

--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,23 @@
-$('.slider-canales').slick();
+$(document).ready(function () {
+  const config = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+
+    responsive: [{
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  }
+  $('.slider-canales').slick(config);
+});
