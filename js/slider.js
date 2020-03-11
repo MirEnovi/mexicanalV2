bb6590e471ff8d7cf9426e46d8941555ac70ce1d
Mirian <mir@teoapp.co>
2020-03-10 22:42:21 -0600

    actualiza proyecto

--- js/slider.js
+++ js/slider.js
@@ -1,46 +1,31 @@
+const configHome = {
+  arrows: true,
+  infinite: true,
+  speed: 1000,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+
+  responsive: [{
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+}
+
 $(document).ready(function () {
-  const config = {
-    arrows: false,
-    pauseOnFocus: false,
-    pauseOnHover: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 2,
-    autoplay: true,
-    autoplaySpeed: 3000,
-
-    responsive: [{
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
-  }
   $('.slider-canales').slick({
-    arrows: false,
     infinite: true,
-    speed: 1000,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-
-    responsive: [{
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
+  });
+  $('.slider-pd').slick({
+  infinite: true,
   });
 });
+
