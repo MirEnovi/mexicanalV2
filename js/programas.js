a3fa1501375423aa5dd9fd567d948964b65f3415
Mirian <mir@teoapp.co>
2020-03-13 02:57:18 -0600

    ajustes a pagina programa seleccionado

---
+++ js/programas.js
@@ -0,0 +1,6 @@
+$(document).ready(()=>{
+  $('.selecion-programa').on('click', () => {
+    $('#programa-seleccionado-id').css('display', 'block');
+    $('#slider-programas-id').css('display', 'none');
+  })
+})
