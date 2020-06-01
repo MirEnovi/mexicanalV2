f0cbd6929bdd24826a1feae479f0ed147ad4740e
Mirian <mir@teoapp.co>
2020-06-01 01:32:04 -0500

    agrega logica para calandario y slider

--- js/programacion.js
+++ js/programacion.js
@@ -1,3 +1,104 @@
+
+
+// CALENDARIO
+const mesDom = document.getElementById('mes');
+const yearDom = document.getElementById('year');
+const diasDom = document.getElementById('dias');
+
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+
+const fecha = new Date();
+const hoy = fecha.getDate() + "/" + (fecha.getMonth() +1) + "/" + fecha.getFullYear();
+
+const meses = ['ENERO', 'FEBRERO', 'MARZO', 'ABRIL', 'MAYO', 'JUNIO', 'JULIO', 'AGOSTO', 'SEPTIEMBRE', 'OCTUBRE', 'NOVIEMBRE', 'DICIEMBRE'];
+
+const month = ['JANUARY', 'FEBRUARY', 'MARCH', 'APRIL', 'MAY', 'JUNE', 'JULY', 'AUGUST', 'SEPTEMBER', 'OCTOBER', 'NOVEMBER', 'DECEMBER'];
+const diasEnUnMes = (mes, año) => {
+	return new Date(año, mes, 0).getDate();
+}
+
+const rellenarCalendario = (mes, year, dia) => {
+	const diasDeMes = diasEnUnMes((mes+1), year);
+	const diaUno =  new Date(`${month[mes]} ${1}, ${year}`).getDay();
+	mesDom.innerHTML = `<span class="nombre-mes" id="${mes}">${meses[mes]}</span>`;
+	yearDom.innerHTML = `<span class="text-year" id="${year}">${ year}</span>`;
+	diasDom.innerHTML = '';
+	for (let i = 0; i < (diasDeMes + diaUno); i++) {
+		if (i<diaUno) {
+			diasDom.innerHTML += `
+				<li></li>
+			`
+		} else {
+			// console.log((i+1)-diaUno);
+			if ((i+1)-diaUno == dia ) {
+				diasDom.innerHTML += `
+				<li class="send-day" id="${(i+1)-diaUno}">
+					<span class="active">${(i+1)-diaUno}</span>
+				</li>
+				`
+			} else {
+				diasDom.innerHTML += `
+				<li class="send-day" id="${(i+1)-diaUno}" >${(i+1)-diaUno}</li>
+				`
+			}
+		}
+	}
+
+// TOMA EL ID DEL DIA QUE SELECCIONO Y ENVIAR
+	$('.send-day').on('click', (e) => {
+		e.preventDefault();
+		const mes = document.querySelector('.nombre-mes');
+		const year = document.querySelector('.text-year');
+		rellenarCalendario(mes.id, year.id, e.target.id);
+
+		const seleccionFecha = `${e.target.id}/${parseInt(mes.id)+1}/${year.id}`
+		console.log(seleccionFecha);
+	})
+}
+
+prev.addEventListener('click', (e)=>{
+	e.preventDefault();
+	const mes = document.querySelector('.nombre-mes');
+	const year = document.querySelector('.text-year');
+
+	if (mes.id === '0' ) {
+		rellenarCalendario(`${parseInt(mes.id) + 11}`, (year.id-1), 1);
+	} else {
+		rellenarCalendario((mes.id-1), (year.id), 1);
+	}
+})
+
+next.addEventListener('click', (e)=>{
+	e.preventDefault();
+	const mes = document.querySelector('.nombre-mes');
+	const year = document.querySelector('.text-year');
+	const mesHoy = fecha.getMonth();
+	const yearHoy = fecha.getFullYear();
+
+	console.log(mes.id, mesHoy);
+	if (mes.id == parseInt(mesHoy)+1  && year.id == yearHoy) {
+		console.log('no hay nada adelante');
+		alert('Lo sentimos, no hay programación más adelante')
+	} else {
+		if (mes.id === '11' ) {
+			rellenarCalendario(`${parseInt(mes.id) - 11}`, (parseInt(year.id)+1), 1);
+		} else {
+			rellenarCalendario((parseInt(mes.id)+1), (year.id), 1);
+		}
+	}
+});
+
+const getMes = fecha.getMonth();
+const getYear = fecha.getFullYear();
+const getDia = fecha.getDate();
+rellenarCalendario(getMes, getYear, getDia);
+
+
+// FIN CALENDARIO
+
+
+
 $(document).ready(() => {
 	$('.nav-zona').on('click', (e) => {
 		e.preventDefault();
@@ -12,9 +113,11 @@ $(document).ready(() => {
 			cambioClase(e.target.id, 'pacifico')
 		}
 	});
+
+
+
 	$('#clandario-show').on('click',(e)=>{
 		e.preventDefault();
-		console.log('calendario');
 		$("#calendario").toggle();
 	})
 	$('#busqueda-show').on('click',(e)=>{
@@ -22,6 +125,7 @@ $(document).ready(() => {
 		console.log('busqueda');
 		$("#busqueda").toggle();
 	})
+
 })
 
 const cambioClase = (id, zona) => {
--- js/programas.js
+++ js/programas.js
@@ -1,36 +1,77 @@
-
 $(document).ready(function () {
-  (function ($) {
-    $('.selecion-programa').on('click', () => {
-      $('#programa-seleccionado-id').css('display', 'block');
-      $('#slider-programas-id').css('display', 'none');
-    })
+	(function ($) {
+		$('.selecion-programa').on('click', () => {
+			$('#programa-seleccionado-id').css('display', 'block');
+			$('#slider-programas-id').css('display', 'none');
+		})
 
-    // slider
-    $('.owl-carousel').owlCarousel({
-      loop: true,
-      lazyLoad: true,
-      margin: 0,
-      responsiveClass: true,
-      autoplay: true,
-      autoplayTimeout: 5000,
-      autoplayHoverPause: false,
-      nav: false,
-      dots: false,
-      responsive: {
-        0: {
-          items: 1,
-          loop: true
-        },
-        600: {
-          items: 3,
-          loop: true
-        },
-        1000: {
-          items: 5,
-          loop: true
-        }
-      }
-    })
-  })(jQuery);
+		// slider
+		$('.owl-carousel').owlCarousel({
+			loop: true,
+			lazyLoad: true,
+			margin: 0,
+			responsiveClass: true,
+			autoplay: true,
+			autoplayTimeout: 5000,
+			autoplayHoverPause: false,
+			nav: false,
+			dots: false,
+			responsive: {
+				0: {
+					items: 1,
+					loop: true
+				},
+				600: {
+					items: 3,
+					loop: true
+				},
+				1000: {
+					items: 5,
+					loop: true
+				}
+			}
+		})
+	})(jQuery);
 });
+
+const titleElement = document.querySelector('.titulo-programa-selec');
+const domRectTitle = titleElement.getBoundingClientRect();
+let offsetWidth =titleElement.offsetWidth;
+let fifftyposition = Math.round(domRectTitle.x) + Math.round(offsetWidth/2);
+
+const elementsSlider = document.querySelector('.owl-carousel');
+// Opcions para el observer 
+const observerOptions = {
+	attributes: true,
+	childList: true,
+	subtree: true,
+	characterData: false,
+	attributeOldValue: false,
+	characterDataOldValue: false
+};
+
+
+const observer = new MutationObserver(() => {
+	observer.disconnect();
+	const images = document.querySelectorAll('.owl-lazy');
+	images.forEach(image => {
+		
+		if (!image.classList.contains('filter')) {
+			image.classList.add('filter');
+		}
+		const domRectImage = image.getBoundingClientRect();
+		const offsetWidthImage = image.offsetWidth;
+		const fifftypositionImage = Math.round(domRectImage.x) + Math.round(offsetWidthImage/2);
+		if ((fifftyposition + Math.round(offsetWidthImage)) === fifftypositionImage) {
+			console.log(image.id);
+			image.classList.remove('filter');
+		}
+	});
+	observer.observe(elementsSlider, observerOptions)
+});
+
+
+
+setTimeout(() =>{ observer.observe(elementsSlider, observerOptions) }, 1000);
+
+
